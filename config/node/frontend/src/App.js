ce4f57ff87e3366ab8fb672ac66211d21ee0c471
vequr <u56175@student.wat.edu.pl>
2025-12-08 19:28:45 +0100

    feat: landing page architecture draft prepared

--- config/node/frontend/src/App.js
+++ config/node/frontend/src/App.js
@@ -1,10 +1,12 @@
 
-import './App.css';
+import Home from "./pages/Home";
+import './styles/style.scss';
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+        <Home/>
     </div>
   );
 }
