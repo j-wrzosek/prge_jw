b69a8f5d57a57f532f69465b0c3f98d997f95567
vequr <u56175@student.wat.edu.pl>
2025-12-08 19:59:02 +0100

    feat: input of home component and style for it

--- config/node/frontend/src/pages/Home.js
+++ config/node/frontend/src/pages/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Typography, Button} from '@mui/material';
 
 function Home(props) {
     return (
-        <div>strona startowa react</div>
+        <div className='home'>
+            <h1 className='home__title'>GEOPORTAL</h1>
+
+            <Typography className='home__subtitle'>
+                Geoportal tematyczny poświęcony danym przestrzennym.
+            </Typography>
+
+            <Button className='home__button' variant='contained' size='large'> START </Button>
+
+        </div>
     );
 }
 
