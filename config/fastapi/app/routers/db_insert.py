85a77795b5f0485d26e2a1bf59f24d508dc272bb
vequr <u56175@student.wat.edu.pl>
2025-12-17 12:53:25 +0100

    feat: fastapi external environment settings add

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -1,26 +1,26 @@
 from fastapi import APIRouter
 from sqlalchemy import create_engine
 
-
+from app.settings import db_name, db_user, db_password
 
 router_insert = APIRouter()
 
 
-def connect_to_db():
+def connect_to_db(db_name: str, db_user: str, db_password: str):
     return create_engine(
-        f"postgresql://postgres:postgres@postgis:5432/postgres"
+        f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}"
     )
 
 @router_insert.get("/insert_user")
 async def insert_user():
 
     try:
-        db_connection = connect_to_db()
+        db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
 
-        sql_query = ("""
-        insert into users (name, posts, location)
-        values ('Jan', 3, 'Gdańsk'); \
-        """)
+        sql_query = text("""
+                        insert into users (name, posts, location)
+                        values ('Jan', 3, 'Gdańsk'); \
+                        """)
 
         with db_connection.connect() as conn:
             result = conn.execute(sql_query)
