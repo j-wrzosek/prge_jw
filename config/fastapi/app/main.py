16e1d77adf2fcb3d22c62294484068f4c4591025
vequr <u56175@student.wat.edu.pl>
2026-01-12 14:13:17 +0100

    feat: dodane card component

--- config/fastapi/app/main.py
+++ config/fastapi/app/main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 from app.routers.static_content import router as static_router
 from app.routers.db_insert import router as insert_router
 from app.routers.dynamic_content import router_get_users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Mapbook API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 app.include_router(static_router, prefix="/app")
 app.include_router(insert_router, prefix="/app")
